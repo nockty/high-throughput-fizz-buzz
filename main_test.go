3754a8546cdd8fb0193a68e4c83d97753c5dea7e
Corentin Dupret <corentin.dupret@datadoghq.com>
2023-11-05 21:42:07 +0100

    Add benchmark

--- main_test.go
+++ main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/fs"
 	"os"
 	"strings"
 	"testing"
@@ -18,5 +19,17 @@ func TestFizzBuzz(t *testing.T) {
 	actual := b.String()
 	if expected != actual {
 		panic(fmt.Sprintf("unexpected output:\n%s", actual))
+	}
+}
+
+func BenchmarkFizzBuzz100k(b *testing.B) {
+	f, err := os.OpenFile(os.DevNull, os.O_WRONLY, fs.ModeAppend)
+	if err != nil {
+		panic(err)
+	}
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		fizzBuzz(f, 100_000)
 	}
 }
